b18397505f9ec5bef56c06d0b2fcd0e3f63f81ad
Niols <niols@niols.fr>
2021-02-16 19:52:17 +0100

    mechanism to align columns

--- bench/array/timsort.ml
+++ bench/array/timsort.ml
@@ -8,30 +8,83 @@ let spf = Format.sprintf
 let pp_int = Format.pp_print_int
 let pp_string = Format.pp_print_string
 
-type cell = Cell : (unit -> 'a) * (Format.formatter -> 'a -> unit) -> cell
+type cell = Cell : (unit -> 'a) * ('a -> string) -> cell
 type row = cell list
 type table = row list
 
-let cell ~pp f = Cell (f, pp)
+let cell ~to_string f = Cell (f, to_string)
 
-let fmt_cell ~fmt f = cell ~pp:(fun ppf x -> fpf ppf fmt x) f
+let fmt_cell ~fmt f = cell ~to_string:(spf fmt) f
 
-let int_cell i = cell ~pp:pp_int (fun () -> i)
-let string_cell s = cell ~pp:pp_string (fun () -> s)
+let int_cell i = cell ~to_string:string_of_int (fun () -> i)
+let string_cell s = cell ~to_string:Fun.id (fun () -> s)
 
-let pp_cell fmt = function
-  | Cell (f, pp) -> pp fmt (f ())
+type cell_size = None | Given of int | Auto of int ref
+type cell_align = Left | Center | Right
 
-let pp_row fmt = function
+let pp_cell ~size ~align fmt = function
+  | Cell (f, to_string) ->
+    let str = to_string (f ()) in
+    let len = String.length str in
+    let size =
+      match size with
+      | None -> len
+      | Auto size -> size := max !size len; !size
+      | Given size -> size
+    in
+    let str =
+      match align with
+      | Left -> str ^ (String.make (size - len) ' ')
+      | Right -> (String.make (size - len) ' ') ^ str
+      | Center -> (String.make ((size - len) / 2) ' ') ^ str ^ (String.make (size - len - (size - len) / 2) ' ')
+    in
+    pp_string fmt str
+
+let pp_row ~sep_len ?cell_styles fmt cells =
+  match cells with
   | [] -> ()
   | cell :: cells ->
-    pp_cell fmt cell;
-    List.iter (fun cell -> fpf fmt " @?"; pp_cell fmt cell) cells;
+    let ((size, align), styles) =
+      match cell_styles with
+      | None -> ((None, Left), List.map (fun _ -> (None, Left)) cells)
+      | Some ((size, align) :: styles) -> ((size, align), styles)
+      | _ -> invalid_arg "pp_row"
+    in
+    pp_cell ~size ~align fmt cell;
+
+    List.iter2
+      (fun cell (size, align) ->
+         fpf fmt "%s@?" (String.make sep_len ' ');
+         pp_cell ~size ~align fmt cell)
+      cells styles;
     fpf fmt "@."
 
-let pp_table fmt = List.iter (pp_row fmt)
+let pp_table ?title ?headers ?(sep_len=3) ?cell_styles fmt t =
+  let len () =
+    match cell_styles with
+    | None -> 0
+    | Some cell_styles ->
+      List.fold_left
+        (fun len (size, _align) ->
+           len + sep_len
+           + match size with
+             | None -> 0
+             | Auto size -> !size
+             | Given size -> size)
+        (-sep_len) cell_styles
+  in
+  (match title with
+   | None -> ()
+   | Some title -> fpf fmt "============== [ %s ] ==============@." title);
+  (match headers with
+   | None -> ()
+   | Some headers -> pp_row ~sep_len ?cell_styles fmt headers);
+  fpf fmt "%s@." (String.make (len ()) '-');
+  List.iter (pp_row ~sep_len ?cell_styles fmt) t;
+  fpf fmt "%s@." (String.make (len ()) '-')
 
-let print_table t = pp_table Format.std_formatter t
+let print_table ?title ?headers ?cell_styles t =
+  pp_table ?title ?headers ?cell_styles Format.std_formatter t
 
 let log2_fact n =
   let rec log2_fact res = function
@@ -61,13 +114,6 @@ let speedup ~from ~to_ = (from -. to_) /. from *. 100.
 
 let bench_table ~generator ~lengths ~repeat ()
   =
-  let headers =
-    List.map string_cell [
-      "length";
-      "stdlib"; "timsort"; "speedup";
-      "lg(n!)"; "stdlib"; "timsort"; "speedup"
-    ]
-  in
   List.map
     (fun length ->
        let inputs = List.init repeat (fun _ -> generator length) in
@@ -80,24 +126,45 @@ let bench_table ~generator ~lengths ~repeat ()
        [ int_cell length;
          fmt_cell (fun () -> Lazy.force t_std) ~fmt:"%.6f";
          fmt_cell (fun () -> Lazy.force t_ts)  ~fmt:"%.6f";
-         fmt_cell (fun () -> speedup ~from:(Lazy.force t_std) ~to_:(Lazy.force t_ts)) ~fmt:"%.1f";
+         fmt_cell (fun () -> speedup ~from:(Lazy.force t_std) ~to_:(Lazy.force t_ts)) ~fmt:"%.1f%%";
          int_cell (repeat * log2_fact length) ;
          fmt_cell (fun () -> Lazy.force c_std) ~fmt:"%.0f";
          fmt_cell (fun () -> Lazy.force c_ts)  ~fmt:"%.0f";
-         fmt_cell (fun () -> speedup ~from:(Lazy.force c_std) ~to_:(Lazy.force c_ts)) ~fmt:"%.1f" ])
+         fmt_cell (fun () -> speedup ~from:(Lazy.force c_std) ~to_:(Lazy.force c_ts)) ~fmt:"%.1f%%" ])
 
     lengths
-  |> List.cons headers
+
+let headers =
+  List.map string_cell [
+    "length";
+    "stdlib"; "timsort"; "speedup";
+    "lg(n!)"; "stdlib"; "timsort"; "speedup"
+  ]
+
+let cell_styles () =
+  [
+    (Auto (ref 6), Right);
+    (Auto (ref 8), Right);
+    (Auto (ref 8), Right);
+    (Auto (ref 4), Right);
+    (Auto (ref 8), Right);
+    (Auto (ref 8), Right);
+    (Auto (ref 8), Right);
+    (Auto (ref 4), Right);
+  ]
 
 let bench_table =
   let lengths = List.init 15 (fun i -> (i+ 1) * 10000) in
   let repeat = 20 in
   bench_table ~lengths ~repeat
 
-let () =
-  pf "============== [ Unif ] ==============@.";
-  bench_table ~generator:Genarray.gen_unif () |> print_table
 
 let () =
-  pf "============= [ 5-Runs ] =============@.";
-  bench_table ~generator:(Genarray.gen_k_runs 5) () |> print_table
+  bench_table ~generator:Genarray.gen_unif ()
+  |> print_table ~title:"Unif" ~headers
+    ~cell_styles:(cell_styles ())
+
+let () =
+  bench_table ~generator:(Genarray.gen_k_runs 5) ()
+  |> print_table ~title:"5-Runs" ~headers
+    ~cell_styles:(cell_styles ())
