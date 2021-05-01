f6110523ffeffd09d3570a6bdb54b74eef0a61ef
Niols <niols@niols.fr>
2021-05-01 21:32:36 +0200

    powersort!

--- src/array/nearly-optimal-mergesort/PowerSort.ml
+++ src/array/nearly-optimal-mergesort/PowerSort.ml
@@ -12,13 +12,73 @@ let minRunLen = 16
 
 let null_index = Int.min_int
 
+(* FIXME: nodePower, which should be the best implementation, is completely
+   buggy and does not pass the associated tests. In the rest of the code, we
+   will replace it by one of the other nodePower* but this will slow down the
+   algorithm. *)
+
 let nodePower (left: int) (right: int) (startA: int) (startB: int) (endB: int) =
 	let n = right - left + 1 in
 	let l = startA + startB - (left lsl 1) in (* 2*middleA *)
   let r = startB + endB + 1 - (left lsl 1) in (* 2*middleB *)
-  let a = ((l lsl 30) / n) in (* middleA / 2n *)
-  let b = ((r lsl 30) / n) in (* middleB / 2n *)
+  let a = (l lsl 30) / n in (* middleA / 2n *)
+  let b = (r lsl 30) / n in (* middleB / 2n *)
   Base.Int.(clz (a lxor b))
+
+let%test _ = nodePower 1 100 10 20 25 = 4
+let%test _ = nodePower 0 21 8 12 13 = 1
+let%test _ = nodePower 0 21 19 20 20 = 5
+let%test _ = nodePower 0 (100*1000*1000) 55555555 55555666 55556666 = 16
+
+let nodePowerBitwise (left: int) (right: int) (startA: int) (startB: int) (endB: int) =
+	assert (right < (1 lsl 30)); (* otherwise nt2, l and r will overflow *)
+ let n = right - left + 1 in
+ let l = ref (startA - (left lsl 1) + startB) in
+ let r = ref (startB - (left lsl 1) + endB + 1) in
+ (* a and b are given by l/nt2 and r/nt2, both are in [0,1).
+	  we have to find the number of common digits in the
+    binary representation in the fractional part. *)
+ let nCommonBits = ref 0 in
+ let digitA = ref (!l >= n) in
+ let digitB = ref (!r >= n) in
+ while !digitA = !digitB do
+   incr nCommonBits;
+   l := !l - (if !digitA then n else 0);
+   r := !r - (if !digitA then n else 0);
+   l := !l lsl 1;
+   r := !r lsl 1;
+   digitA := !l >= n;
+   digitB := !r >= n
+ done;
+ !nCommonBits + 1
+
+let%test _ = nodePowerBitwise 1 100 10 20 25 = 4
+let%test _ = nodePowerBitwise 0 21 8 12 13 = 1
+let%test _ = nodePowerBitwise 0 21 19 20 20 = 5
+let%test _ = nodePowerBitwise 0 (100*1000*1000) 55555555 55555666 55556666 = 16
+
+let nodePowerLoop (left: int) (right: int) (startA: int) (startB: int) (endB: int) =
+  assert (right < (1 lsl 30)); (* otherwise nt2 will overflow *)
+	let nt2 = (right - left + 1) lsl 1 in (* 2*n *)
+  let n1 = startB - startA in
+  let n2 = endB - startB + 1 in
+  let a = ref ((startA lsl 1) + n1 - (left lsl 1)) in
+  let b = ref ((startB lsl 1) + n2 - (left lsl 1)) in
+  let k = ref 0 in
+  while !b - !a <= nt2 && !a / nt2 = !b / nt2 do
+	  incr k;
+	  a := !a lsl 1;
+    b := !b lsl 1
+  done;
+  !k
+
+let%test _ = nodePowerLoop 1 100 10 20 25 = 4
+let%test _ = nodePowerLoop 0 21 8 12 13 = 1
+let%test _ = nodePowerLoop 0 21 19 20 20 = 5
+let%test _ = nodePowerLoop 0 (100*1000*1000) 55555555 55555666 55556666 = 16
+
+(* FIXME: this should not be necessary *)
+let nodePower = nodePowerBitwise
 
 let powersort (cmp: 'a cmp) (a: 'a array) (left: int) (right: int) =
 	let n = right - left + 1 in
@@ -26,27 +86,28 @@ let powersort (cmp: 'a cmp) (a: 'a array) (left: int) (right: int) =
   let leftRunStart = Array.make lgnPlus2 null_index in
   let leftRunEnd   = Array.make lgnPlus2 0 in
  	let top = ref 0 in
-	let buffer = Array.make n a.(0) in
+  let buffer = Array.make n a.(0) in
+
   let startA = ref left in
   let endA = ref (extendAndReverseRunRight cmp a !startA right) in
-	(* extend to minRunLen *)
-	let lenA = !endA - !startA + 1 in
-	if lenA < minRunLen then
+  (* extend to minRunLen *)
+  let lenA = !endA - !startA + 1 in
+  if lenA < minRunLen then
     (
-			endA := min right (!startA + minRunLen-1);
-			Insertionsort.insertionsort_presorted cmp a !startA !endA lenA
+		  endA := min right (!startA + minRunLen-1);
+		  Insertionsort.insertionsort_presorted cmp a !startA !endA lenA
     );
-	while !endA < right do
-		let startB = !endA + 1 in
+  while !endA < right do
+	  let startB = !endA + 1 in
     let endB = ref (extendAndReverseRunRight cmp a startB right) in
 	  (* extend to minRunLen *)
     let lenB = !endB - startB + 1 in
 	  if lenB < minRunLen then
       (
-				endB := min right (startB + minRunLen-1);
-				Insertionsort.insertionsort_presorted cmp a startB !endB lenB
+			  endB := min right (startB + minRunLen-1);
+			  Insertionsort.insertionsort_presorted cmp a startB !endB lenB
       );
-		let k = nodePower left right !startA startB !endB in
+	  let k = nodePower left right !startA startB !endB in
 	  assert (k <> !top);
     (* for (int l = top; l > k; --l) *)
     let rec for_loop l =
@@ -70,18 +131,26 @@ let powersort (cmp: 'a cmp) (a: 'a array) (left: int) (right: int) =
 	  top := k;
 	  startA := startB;
     endA := !endB;
-	done;
-	assert (!endA = right);
+  done;
+  assert (!endA = right);
   (* for (int l = top; l > 0; --l) *)
   let rec for_loop l =
-	  if leftRunStart.(l) = null_index then for_loop (l-1)
+    if l <= 0 then ()
     else
       (
-        mergeRuns cmp a leftRunStart.(l) (leftRunEnd.(l)+1) right buffer;
-        for_loop (l-1)
+	      if leftRunStart.(l) = null_index then for_loop (l-1)
+        else
+          (
+            mergeRuns cmp a leftRunStart.(l) (leftRunEnd.(l)+1) right buffer;
+            for_loop (l-1)
+          )
       )
   in
   for_loop !top
+
+let powersort (cmp: 'a cmp) (a: 'a array) (left: int) (right: int) =
+  if a = [||] then ()
+  else powersort cmp a left right
 
 (** This function is not given like this in the Java implementation but is here
    for interoperability with the OCaml way of presenting sorting algorithms. *)
